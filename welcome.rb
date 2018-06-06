0bf741f2fcbfd6769164faaf02578dbea8218fcf
EmiKnape <enknape@aol.com>
2018-06-06 16:52:06 +0000

    automatically backed up by learn

--- welcome.rb
+++ welcome.rb
@@ -1,4 +1,4 @@
 def tv_welcome(greeting, name, welcome_message, room_number)
-  puts "hello #{name},Thank you for staying at SpringHill Suites Phoenix Downtown,#{room_number}" 
+  puts "hello #{name}, welocome to #{room_number}"
 end 
 tv_welcome("Jonathan","Room 234")
