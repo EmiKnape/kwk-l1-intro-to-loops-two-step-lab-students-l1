e4ec73d18a12bd8064e480068f41a536d203cdf2
EmiKnape <enknape@aol.com>
2018-06-06 16:42:54 +0000

    automatically backed up by learn

---
+++ welcome.rb
@@ -0,0 +1,4 @@
+def hotel_welcome_greeting (greeting, name, welcome_message, room_number)
+  puts "#{greeting}, #{name},#{welcome_message", #{room_number}"
+end
+hotel_welcome_greeting("hello", "Jonathan", "Thank you for ""Room 234")
