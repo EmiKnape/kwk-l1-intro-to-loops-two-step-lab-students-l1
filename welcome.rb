0b9d1c192c40886135fd5cf8c71a6c95f564ae94
EmiKnape <enknape@aol.com>
2018-06-06 16:52:42 +0000

    automatically backed up by learn

--- welcome.rb
+++ welcome.rb
@@ -1,4 +1,4 @@
-def tv_welcome(greeting, name, welcome_message, room_number)
+def tv_welcome(greeting, name, room_number)
   puts "hello #{name}, welocome to #{room_number}"
 end 
 tv_welcome("Jonathan","Room 234")
