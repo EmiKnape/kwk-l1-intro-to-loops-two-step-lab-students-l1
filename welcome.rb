3ee6dc283a77cee55712b5d6c21df61a35ef660b
EmiKnape <enknape@aol.com>
2018-06-06 16:49:35 +0000

    automatically backed up by learn

--- welcome.rb
+++ welcome.rb
@@ -1,4 +1,4 @@
 def tv_welcome(greeting, name, welcome_message, room_number)
-  puts "hello #{name},#{welcome_message",#{room_number}" 
+  puts "hello #{name},",#{room_number}" 
 end 
-tv_welcome("hello", "Jonathan", "Thank you for staying at SpringHill Suites Phoenix Downtown", "Room 234")
+tv_welcome("hello", "Jonathan", , "Room 234")
