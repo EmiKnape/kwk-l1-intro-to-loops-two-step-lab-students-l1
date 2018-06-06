71c81ca8e507d569b5ebbc61fd89cf5193de2e8e
EmiKnape <enknape@aol.com>
2018-06-06 16:44:15 +0000

    automatically backed up by learn

--- welcome.rb
+++ welcome.rb
@@ -1,4 +1,4 @@
 def hotel_welcome_greeting (greeting, name, welcome_message, room_number)
   puts "#{greeting}, #{name},#{welcome_message", #{room_number}"
 end
-hotel_welcome_greeting("hello", "Jonathan", "Thank you for staying at SpringHill ""Room 234")
+hotel_welcome_greeting("hello", "Jonathan", "Thank you for staying at SpringHill Suites Phoenix Downtown","Room 234")
