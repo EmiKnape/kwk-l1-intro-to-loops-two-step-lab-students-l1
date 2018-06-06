0c0f4a9c745ecec05f675941e33c2ad1a62611ed
EmiKnape <enknape@aol.com>
2018-06-06 16:57:26 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,15 +1,15 @@
-def dance 
-  moves_slayed = 0 
+# def dance 
+#   moves_slayed = 0 
   
-  loop do
-    if moves_slayed == 10 
-    break 
-  end 
-  puts "step to the left"
-  puts "step to the right"
-  puts "kick"
-  moves_slayed = moves_slayed + 1 
-end 
-end 
-dance 
+#   loop do
+#     if moves_slayed == 10 
+#     break 
+#   end 
+#   puts "step to the left"
+#   puts "step to the right"
+#   puts "kick"
+#   moves_slayed = moves_slayed + 1 
+# end 
+# end 
+# dance 
     
